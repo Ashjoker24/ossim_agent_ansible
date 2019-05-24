f0ad592763f7e4749fb5bf43268257a1a82f8a05
Ashjoker24 <43417210+Ashjoker24@users.noreply.github.com>
2019-05-24 16:13:51 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt-get install wget gcc  -y
-if [ -e /tmp/ossec-hids-3.1.0 ]
+if [ -f /tmp/ossec-hids-3.1.0/ ]
 then
 echo "already available"
 else
 wget https://codeload.github.com/ossec/ossec-hids/tar.gz/3.1.0 -P /tmp
-sudo tar xvf /tmp/3.1.0 -C /tmp/
+sudo tar xvf /tmp/3.1.0 -C /tmp
 fi
+rm -rf 3.1.0*
