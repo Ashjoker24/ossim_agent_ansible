25a9b9a6d88e2f1d1080421ac3c356b35a9da206
Ashjoker24 <43417210+Ashjoker24@users.noreply.github.com>
2019-05-24 16:12:54 +0530

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo yum install wget gcc  -y
-if [ -e /tmp/3.1.0 ]
+if [ -f /tmp/ossec-hids-3.1.0/ ]
 then
 echo "already available"
 else
 wget https://codeload.github.com/ossec/ossec-hids/tar.gz/3.1.0 -P /tmp
-sudo tar xvf /tmp/3.1.0
+sudo tar xvf /tmp/3.1.0 -C /tmp
 fi
+rm -rf 3.1.0*
